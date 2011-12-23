a27742383e38ea6227da813b684db44f0358af8d
Giacomo Spettoli <giacomo.spettoli@gmail.com>
2011-12-23 18:28:40 +0100

    Many graphical improvements

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.1'
+version = '1.2'
 
 setup(
         name='plonetheme.bananaleaf',
@@ -11,8 +11,8 @@ setup(
                          open(os.path.join("docs", "HISTORY.txt")).read(),
         version='1.1',
         author='Giacomo Spettoli',
-        author_email='giacomo.spettoli@gmail.com',
-        url='http://svn.plone.org/svn/collective/plonetheme.bananaleaf',
+        author_email='giacomo.spettoli@redomino.com',
+        url='https://github.com/redomino/plonetheme.bananaleaf',
         packages=find_packages(),
         include_package_data=True,
         namespace_packages=[
